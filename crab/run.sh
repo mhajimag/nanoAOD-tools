915000591b7f2f680ad46fe32399339ee0966b0b
Matthew Knight <matthewdknight9@gmail.com>
2021-04-26 12:58:28 +0200

    Running in batch now in working state

--- crab/run.sh
+++ crab/run.sh
@@ -3,22 +3,39 @@
 set -x
 #set -e
 
-export X509_USER_PROXY=$1
-voms-proxy-info -all
-voms-proxy-info -all -file $1
-
+outDir=$1
 in_file=$2
-entries=$3
+rw_path=$3
 start=$4
+entries=$5
 
-cd /eos/home-m/mknight/dev/CMSSW_10_6_8/src/PhysicsTools/NanoAODTools
+if [[ -n $6 ]]; then
+    export X509_USER_PROXY=$6
+    voms-proxy-info -all
+    voms-proxy-info -all -file $6
+fi
+
+if [[ -n $7 && -n $8 ]]; then
+    ClusterId=$7
+    ProcId=$8
+else
+    ClusterId=local
+    ProcId=run
+fi
+
+cd /afs/cern.ch/user/m/mknight/private/CMSSW_10_6_8/src/PhysicsTools/NanoAODTools
 source /cvmfs/cms.cern.ch/cmsset_default.sh 
 eval `scramv1 runtime -sh`
 
-#in_file=root://xrootd-cms.infn.it//store/mc/RunIISummer19UL18NanoAODv2/GluGluHToGG_M125_TuneCP5_13TeV-amcatnloFXFX-pythia8_storeWeights/NANOAODSIM/106X_upgrade2018_realistic_v15_L1v1-v1/40000/9646BBBF-8A18-F345-9E84-A15B6BA628A8.root
-rw_path=/afs/cern.ch/user/m/mknight/private/EFT_Reweighting/rw_modules/rw_ggFfull-SMEFTsim_cHG
+postfix=${ClusterId}_${ProcId}
 
-python scripts/run_reweighting.py ${TMPDIR} ${in_file} ${rw_path} lhe ${entries} 0 ${start}
+python scripts/run_reweighting.py ${TMPDIR} ${in_file} ${rw_path} --first-entry ${start} -N ${entries} -s ${postfix} -m ttH 
 
-cp ${TMPDIR}/9646BBBF-8A18-F345-9E84-A15B6BA628A8_Skim.root /eos/home-m/mknight/dev/CMSSW_10_6_8/src/PhysicsTools/NanoAODTools/ggF_Skim.root
+mkdir -p ${outDir}
+cp ${TMPDIR}/*${postfix}.root ${outDir}/${postfix}.root
 
+#split=$(echo $in_file | tr "/" "\n")
+#for s in $split
+#do
+#    filename=$s
+#done
